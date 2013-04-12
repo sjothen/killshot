991b134f2693ff69b4baab1c69465d98fdba93b2
Stephen Jothen <sjothen@gmail.com>
2013-04-12 19:16:25 +0200

    Move printing stuff to a separate class

--- lib/killshot.rb
+++ lib/killshot.rb
@@ -1,7 +1,6 @@
 require 'killshot/version'
 require 'rubygems'
 require 'anemone'
-require 'colored'
 require 'nokogiri'
 require 'uri'
 require 'set'
@@ -13,42 +12,50 @@ module Killshot
     def initialize(root, whitelist)
       @root      = root
       @whitelist = Set.new(whitelist)
-      @hotlinks  = []
     end
 
-    def crawl!
+    def crawl(&block)
       Anemone.crawl(root) do |anemone|
         anemone.on_every_page do |page|
-          find_hotlinks(page)
+          find_hotlinks(page) do |url, hotlink|
+            block.call(url, hotlink)
+          end
         end
       end
-
-      puts "Done. Found #{@hotlinks.length} hotlinked images!"
     end
 
     private
 
-    def hotlink?(img)
-      src  = img['src']
-      uri = URI(URI::escape(src))
+    def hotlink?(imgsrc)
+      uri = URI(URI::escape(imgsrc))
       # Check if absolute, ignore relative links
       uri.absolute? && !whitelist.member?(uri.host)
     end
 
-    def hotlink_found(page, img)
-      src = img['src']
-      url = page.url.to_s.green
-      img = img['src'].red
-
-      @hotlinks << src
-      puts "#{@hotlinks.length}. #{img} from #{url}"
-    end
-
-    def find_hotlinks(page)
+    def find_hotlinks(page, &block)
       doc = Nokogiri::HTML(page.body)
       doc.xpath("//img").each do |img|
-        hotlink_found(page, img) if hotlink?(img)
+        block.call(page.url.to_s, img['src']) if hotlink?(img['src'])
       end
     end
+  end
+
+  class Printer
+    def initialize
+      @count = 0 
+    end 
+
+    def print(url, img)
+      @count = @count + 1 
+      puts "#@count. #{img.red} from #{url.green}"
+    end 
+
+    def start
+      puts "Searching for hotlinks..."
+    end 
+
+    def finish
+      puts "Done. Found #@count hotlinks."
+    end 
   end
 end
